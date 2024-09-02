cf59f06d304bd8a2fac7e50c0663c96235f34184
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-09-02 22:31:01 +0530

    added: docker-compose file

--- Full-stack/Backend/index.js
+++ Full-stack/Backend/index.js
@@ -3,7 +3,10 @@ import fs from "fs";
 import path from "path";
 import cors from "cors";
 import { fileURLToPath } from "url";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+
+// const express = require("express");
+// const cors = require("cors");
 
 const mySecret = "shashwat1234";
 
@@ -25,39 +28,48 @@ const readData = () => {
   return [];
 };
 
-// Function to write data to the JSON file
-const writeData = (data) => {
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 3));
-};
+// // Function to write data to the JSON file
+// const writeData = (data) => {
+//   fs.writeFileSync(filePath, JSON.stringify(data, null, 3));
+// };
 
-app.post('/signup', (req, res) => {
-  let { name, email, password } = req.body;
-  const existingData = readData();
+// app.post('/signup', (req, res) => {
+//   let { name, email, password } = req.body;
+//   const existingData = readData();
 
-  const bcryptPassword = bcrypt.hashSync(password, 10);
-  const newData = {
-    id: Math.floor(Math.random() * 1000).toString(),
-    name,
-    email,
-    password: bcryptPassword
-  }
-  console.log(newData)
-  const updatedData = [...existingData, newData];
-  writeData(updatedData);
-  res.status(201).json(newData);
-});
+//   const bcryptPassword = bcrypt.hashSync(password, 10);
+//   const newData = {
+//     id: Math.floor(Math.random() * 1000).toString(),
+//     name,
+//     email,
+//     password: bcryptPassword
+//   }
+//   console.log(newData)
+//   const updatedData = [...existingData, newData];
+//   writeData(updatedData);
+//   res.status(201).json(newData);
+// });
 
-app.post('/signin', (req, res) => {
-  const { email, password } = req.body;
-  const data = readData();
-  const user = data.find(user => user.email === email && bcrypt.compareSync(password, user.password));
-  if (user) {
-    res.status(200).json({
-      message: "login success",
-    });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
-  }
+// app.get('/', (req, res) => {
+//   res.send("welcome to the node world")
+// })
+
+// app.post('/signin', (req, res) => {
+//   const { email, password } = req.body;
+//   const data = readData();
+//   const user = data.find(user => user.email === email && bcrypt.compareSync(password, user.password));
+//   if (user) {
+//     res.status(200).json({
+//       message: "login success",
+//     });
+//   } else {
+//     res.status(401).json({ message: 'Invalid credentials' });
+//   }
+// })
+
+app.get("/", (req, res) => {
+  const data = readData()
+  res.status(200).json(data)
 })
 
 
